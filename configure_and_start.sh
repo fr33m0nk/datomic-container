9aa891d88a1d6b4e063ae58e13d18a8a14c22029
fr33m0nk <prashantsinha@protonmail.com>
2024-02-06 23:33:19 +0530

    Adds support for `BACKUP_DB`, `RESTORE_DB`, `VERIFY_BACKUP` and `LIST_BACKUPS`

--- configure_and_start.sh
+++ configure_and_start.sh
@@ -11,7 +11,7 @@ add_config() {
   echo "$1=$2" | tee -a sql-transactor.properties
 }
 
-if [[ "${RUN_MODE}" =~ ^(TRANSACTOR|PEER)$ ]]; then
+if [[ "${RUN_MODE}" =~ ^(TRANSACTOR|PEER|BACKUP|RESTORE)$ ]]; then
   echo "Invalid RUN_MODE supplied.\nTRANSACTOR and PEER are the only supported values."
   exit 1
 fi
@@ -48,4 +48,39 @@ if [[ "${RUN_MODE}" = "PEER" ]]; then
           -p "${PEER_PORT}"\
           -a "${PEER_ACCESSKEY}","${PEER_SECRET}"\
           -d "${DATOMIC_DB_NAME}","datomic:sql://${DATOMIC_DB_NAME}?jdbc:postgresql://${PG_HOST}:${PG_PORT}/${PG_DATABASE}?user=${PG_USER}&password=${PG_PASSWORD}"
+fi
+
+if [[ "${RUN_MODE}" = "BACKUP_DB" ]]; then
+  validate_env_vars "${BACKUP_S3_BUCKET_URI}" "BACKUP_S3_BUCKET_URI"
+  validate_env_vars "${DATOMIC_DB_NAME}" "DATOMIC_DB_NAME"
+
+  bin/datomic -Xmx"$XMX" -Xms"$XMS" backup-db "datomic:sql://${DATOMIC_DB_NAME}?jdbc:postgresql://${PG_HOST}:${PG_PORT}/${PG_DATABASE}?user=${PG_USER}&password=${PG_PASSWORD}" "$BACKUP_S3_BUCKET_URI"
+fi
+
+if [[ "${RUN_MODE}" = "LIST_BACKUPS" ]]; then
+  validate_env_vars "${BACKUP_S3_BUCKET_URI}" "BACKUP_S3_BUCKET_URI"
+
+  bin/datomic -Xmx"$XMX" -Xms"$XMS" list-backups "$BACKUP_S3_BUCKET_URI"
+fi
+
+if [[ "${RUN_MODE}" = "VERIFY_BACKUP" ]]; then
+  validate_env_vars "${BACKUP_S3_BUCKET_URI}" "BACKUP_S3_BUCKET_URI"
+  validate_env_vars "${BACKUP_TIME_IN_LONG}" "BACKUP_TIME_IN_LONG"
+
+  if [[ -z "${VERIFY_ALL_SEGMENTS}" ]]; then
+    bin/datomic -Xmx"$XMX" -Xms"$XMS" verify-backup "${BACKUP_S3_BUCKET_URI}" "false" "${BACKUP_TIME_IN_LONG}"
+  else
+    bin/datomic -Xmx"$XMX" -Xms"$XMS" verify-backup "${BACKUP_S3_BUCKET_URI}" "true" "${BACKUP_TIME_IN_LONG}"
+  fi
+fi
+
+if [[ "${RUN_MODE}" = "RESTORE_DB" ]]; then
+  validate_env_vars "${BACKUP_S3_BUCKET_URI}" "BACKUP_S3_BUCKET_URI"
+  validate_env_vars "${DATOMIC_DB_NAME}" "DATOMIC_DB_NAME"
+
+  if [[ -z "${RESTORE_TIME_IN_LONG}" ]]; then
+    bin/datomic -Xmx"$XMX" -Xms"$XMS" restore-db "${BACKUP_S3_BUCKET_URI}" "datomic:sql://${DATOMIC_DB_NAME}?jdbc:postgresql://${PG_HOST}:${PG_PORT}/${PG_DATABASE}?user=${PG_USER}&password=${PG_PASSWORD}"
+  else
+    bin/datomic -Xmx"$XMX" -Xms"$XMS" restore-db "${BACKUP_S3_BUCKET_URI}" "datomic:sql://${DATOMIC_DB_NAME}?jdbc:postgresql://${PG_HOST}:${PG_PORT}/${PG_DATABASE}?user=${PG_USER}&password=${PG_PASSWORD}" "${BACKUP_TIME_IN_LONG}"
+  fi
 fi
