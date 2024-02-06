3d8301d68bdc7457442911d1da8e1c459a1d004f
fr33m0nk <prashantsinha@protonmail.com>
2024-02-06 13:34:15 +0530

    Adds support for Datomic Peer

--- configure_and_start.sh
+++ configure_and_start.sh
@@ -17,17 +17,30 @@ validate_env_vars "${PG_USER}" "PG_USER"
 validate_env_vars "${PG_PASSWORD}" "PG_PASSWORD"
 validate_env_vars "${PG_DATABASE}" "PG_DATABASE"
 
-## Prepare config file for Datomic Transactor
-add_config "protocol" "sql"
-add_config "host" "${TRANSACTOR_HOST}"
-add_config "port" "${TRANSACTOR_PORT}"
-add_config "sql-url" "jdbc:postgresql://${PG_HOST}:${PG_PORT}/${PG_DATABASE}"
-add_config "sql-user" "${PG_USER}"
-add_config "sql-password" "${PG_PASSWORD}"
-add_config "sql-driver-class" "org.postgresql.Driver"
-add_config "memory-index-threshold" "32m"
-add_config "memory-index-max" "512m"
-add_config "object-cache-max" "1g"
+if [[ "${RUN_MODE}" = "TRANSACTOR" ]]; then
 
-## Start up Datomic Transactor
-bin/transactor -Xmx"$XMX" -Xms"$XMS" sql-transactor.properties
+  ## Prepare config file for Datomic Transactor
+  add_config "protocol" "sql"
+  add_config "host" "${TRANSACTOR_HOST}"
+  add_config "port" "${TRANSACTOR_PORT}"
+  add_config "sql-url" "jdbc:postgresql://${PG_HOST}:${PG_PORT}/${PG_DATABASE}"
+  add_config "sql-user" "${PG_USER}"
+  add_config "sql-password" "${PG_PASSWORD}"
+  add_config "sql-driver-class" "org.postgresql.Driver"
+  add_config "memory-index-threshold" "32m"
+  add_config "memory-index-max" "512m"
+  add_config "object-cache-max" "1g"
+
+  ## Start up Datomic Transactor
+  bin/transactor -Xmx"$XMX" -Xms"$XMS" sql-transactor.properties
+fi
+
+if [[ "${RUN_MODE}" = "PEER" ]]; then
+  validate_env_vars "${DATOMIC_DB_NAME}" "DATOMIC_DB_NAME"
+
+  bin/run -m datomic.peer-server\
+          -h "${PEER_HOST}"\
+          -p "${PEER_PORT}"\
+          -a "${PEER_ACCESSKEY}","${PEER_SECRET}"\
+          -d ${DATOMIC_DB_NAME},"datomic:sql://${DATOMIC_DB_NAME}?jdbc:postgresql://${PG_HOST}:${PG_PORT}/${PG_DATABASE}?user=${PG_USER}&password=${PG_PASSWORD}"
+fi
