fbf6531cfa1cf25237eb8df8df5adb781f6f8aff
fr33m0nk <prashantsinha@protonmail.com>
2024-02-06 11:38:07 +0530

    Removes validations from startup script

--- configure_and_start.sh
+++ configure_and_start.sh
@@ -4,20 +4,15 @@ add_config() {
   echo "$1=$2" | tee -a sql-transactor.properties
 }
 
-if [[ ! -z "$PG_HOST" ]]; then
+add_config "protocol" "sql"
+add_config "host" "${TRANSACTOR_HOST}"
+add_config "port" "${TRANSACTOR_PORT}"
+add_config "sql-url" "jdbc:postgresql://${PG_HOST}:${PG_PORT}/${PG_DATABASE}"
+add_config "sql-user" "${PG_USER}"
+add_config "sql-password" "${PG_PASSWORD}"
+add_config "sql-driver-class" "org.postgresql.Driver"
+add_config "memory-index-threshold" "32m"
+add_config "memory-index-max" "512m"
+add_config "object-cache-max" "1g"
 
-  add_config "protocol" "sql"
-  add_config "host" "${TRANSACTOR_HOST}"
-  add_config "port" "${TRANSACTOR_PORT}"
-  add_config "sql-url" "jdbc:postgresql://${PG_HOST}:${PG_PORT}/${PG_DATABASE}"
-  add_config "sql-user" "${PG_USER}"
-  add_config "sql-password" "${PG_PASSWORD}"
-  add_config "sql-driver-class" "org.postgresql.Driver"
-  add_config "memory-index-threshold" "32m"
-  add_config "memory-index-max" "512m"
-  add_config "object-cache-max" "1g"
-  bin/transactor -Xmx"$XMX" -Xms"$XMS" sql-transactor.properties
-
-else
-  echo "PG_HOST cannot be empty. Terminating process"
-fi
+bin/transactor -Xmx"$XMX" -Xms"$XMS" sql-transactor.properties
